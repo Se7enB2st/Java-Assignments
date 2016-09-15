a109ba1c8e826b34f8eae0657e61855f9aa7aeba
Ethan He <ethanhe1992@gmail.com>
2016-09-15 17:13:37 -0400

    Update BatchGrayscale.java

--- BatchGrayscale.java
+++ BatchGrayscale.java
@@ -1,3 +1,11 @@
+//Duke University Java Course Assignment: Batch Grayscale
+
+//Your program should let the user select multiple image files
+//For each image, create a new image that is a grayscale version of the original image
+//For each image, save the grayscale image in a new file with the same filename as the original image,
+//but with the word “gray-” in front of the filename. For example, if the original file was named lion.png,
+//the new image would be a grayscale image and be named gray-lion.png.
+
 import edu.duke.*;
 import java.io.*;
 
