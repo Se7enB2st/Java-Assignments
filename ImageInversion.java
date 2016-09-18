d0f8f3e00c16b694a80b5a2beb27311c167fb37e
Ethan He <ethanhe1992@gmail.com>
2016-09-18 12:04:03 -0400

    Update ImageInversion.java

--- ImageInversion.java
+++ ImageInversion.java
@@ -8,30 +8,35 @@
 //Note that 255 - 34 is 221, 255 - 198 is 57, and 255 - 240 is 15.
 
 import edu.duke.*;
-import java.io.*;
-public class imageInversions {
+import java.io.File;
+
+public class BatchInversions {
     public ImageResource makeInversion(ImageResource inImage) {
-        ImageResource outImage = new ImageResource(inImage.getWidth(), inImage.getHeight());
+        ImageResource outImage = new ImageResource(inImage);
         for (Pixel pixel: outImage.pixels()) {
+            
             Pixel inPixel = inImage.getPixel(pixel.getX(), pixel.getY());
-            Pixel outPixelRed = outPixelRed.setRed(255 - inImage.getRed());
-            Pixel outPixelBlue = outPixeBlue.setBlue(255 - inImage.getBlue());
-            Pixel outPixelGreen = outPixelGreen.setGreen(255 - inImage.getGreen());
-        }
+            
+            int invRed = 255 - inPixel.getRed();
+            int invGreen = 255 - inPixel.getGreen();
+            int invBlue = 255 - inPixel.getBlue();
+            
+            pixel.setRed(invRed);
+            pixel.setGreen(invGreen);
+            pixel.setBlue(invBlue);
+        }  
         return outImage;
     }
     
     public void selectAndConvert() {
         DirectoryResource dr = new DirectoryResource();
-        for (File f: dr.selectedFiles()){
-        ImageResource image = new ImageResource(f);
-        ImageResource invertImage = makeInversion(f);
-        String fileName = image.getFileName();
-        String newFileName = "invert-" + fileName;
-        invertImage.setFileName(newFileName);
-        invertImage.draw();
-        invertImage.save();
-    }
-}
+        for (File f : dr.selectedFiles()) {
+            ImageResource inImage = new ImageResource(f);
+            ImageResource imageInv = makeInversion(inImage);
+            imageInv.setFileName("inverted-" + inImage.getFileName());
+            imageInv.draw();
+            imageInv.save();
+        }
+    }   
 }
 
