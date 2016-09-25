d8be87a118788960caedb688e2fcd62a372dca9b
Ethan He <ethanhe1992@gmail.com>
2016-09-25 13:05:51 -0400

    Update friendConnection.java

--- friendConnection.java
+++ friendConnection.java
@@ -4,12 +4,13 @@
 
 public class Person
 {
-    //
+    // Instance variables
     private String name;
     private String friends;
     private int x;
     private int y;
-
+    
+    // Person constructor
     public Person (String aName, String pictureName, int xCoord, int yCoord)
     {
         name = aName;
@@ -23,20 +24,25 @@ public class Person
     
     public void addFriend(Person friend) 
     {
+        // Add friend's name onto friend list
         friends = friends + friend.name + " ";
+        // Draw a circle on top of person
         SmallCircle circle = new SmallCircle(x, y);
         circle.fill();
+        // connect the circle to friends
         Line friendLine = new Line(x, y, friend.x, friend.y);
         friendLine.draw();
     }
     
     public void unfriend(Person nonFriend)
     {
+        // remove friend's name from list 
         friends = friends.replace(nonFriend.name + " ", "");
     }
     
     public String getFriends() 
     {
+        // return list of friend's names
         return friends;
     }
 }
