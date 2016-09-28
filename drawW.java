62b6b50ac54239a0a7dd974cb14c3b5bcff091a8
Ethan He <ethanhe1992@gmail.com>
2016-09-28 13:11:42 -0400

    Create drawW.java

---
+++ drawW.java
@@ -0,0 +1,8 @@
+public class WViewer {
+    public static void main(String[] args) {
+        new Line(0,0,10,30).draw();
+        new Line(10,30,20,0).draw();
+        new Line(20,0,30,30).draw();
+        new Line(30,30,40,0).draw();    
+    }
+}
