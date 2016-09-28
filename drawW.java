99d1113696c8598e0b2287de82c5cb3078774438
Ethan He <ethanhe1992@gmail.com>
2016-09-28 13:12:44 -0400

    Update drawW.java

--- drawW.java
+++ drawW.java
@@ -1,3 +1,11 @@
+//San Jose State University Intro to Java Course Assignment: Draw W
+
+// Write the code to draw a symmetrical letter W with 4 line segments.  
+// Start the upper left hand corner at (0,0) 
+// The total width will be 40 and the height will be 30.
+// The drawing should look like this:
+// http://i.imgur.com/kIwzrNn.png
+
 public class WViewer {
     public static void main(String[] args) {
         new Line(0,0,10,30).draw();
