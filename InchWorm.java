41d2b15058e03e5ddaccced3fb427128106d0047
Ethan He <ethanhe1992@gmail.com>
2016-09-30 15:09:21 -0400

    Create InchWorm.java

---
+++ InchWorm.java
@@ -0,0 +1,15 @@
+//San Jose State University Intro to Java Assignment: Inch Worm
+
+// Write a class InchWorm that models an inch worm moving along a 
+// horizontal line. The inch worm  moves either to the right or left.
+// Initially, the inch worm moves to the right, but it can turn to
+// change its direction. In each move, its position changes by one
+// unit in the current direction. 
+
+// Provide two constructors. 
+// One that takes the initial position as an argument,
+// and one with no arguments that sets the initial position to 0. 
+
+// You will need two instance variables. 
+// One to keep track of the position and one to keep track of the direction. 
+// You can use 1 to represent moving right and -1 to represent moving left. 
