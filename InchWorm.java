6df27e97dbb92f797961f36e0fa2b7ba7cf26f78
Ethan He <ethanhe1992@gmail.com>
2016-09-30 23:10:15 -0400

    Update InchWorm.java

--- InchWorm.java
+++ InchWorm.java
@@ -13,3 +13,36 @@
 // You will need two instance variables. 
 // One to keep track of the position and one to keep track of the direction. 
 // You can use 1 to represent moving right and -1 to represent moving left. 
+
+public class InchWorm {
+    // TODO: Add instance variables
+    private int position;
+    private int direction;
+
+    public InchWorm() {
+        // TODO: Complete constructor
+        direction = 1;
+        position = 0;
+    }
+
+    public InchWorm (int initialPosition) {
+        // TODO: set the instance variable to the constructor parameter
+        this.position = initialPosition;
+        this.direction = 1;
+    }
+
+    public void move() {
+        // TODO: Update the position of the inch worm
+        this.position += this.direction;
+    }
+
+    public void turn() {
+        // TODO: Update the direction
+        // =- means put negative in front of number
+        this.direction =-  this.direction;
+    }
+
+    public int getPosition() { return this.position; }
+
+    public int getDirection() { return this.direction; }
+}
