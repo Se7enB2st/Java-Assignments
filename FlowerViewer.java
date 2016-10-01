dcdc107797d2f2eb33db9be0390522a8c74bb5e3
Ethan He <ethanhe1992@gmail.com>
2016-09-30 23:12:11 -0400

    Create FlowerViewer.java

---
+++ FlowerViewer.java
@@ -0,0 +1,18 @@
+//San Jose State University Intro to Java Assignment: Flower
+
+// Write a class that describes a flower. A flower has a location, a
+// constructor and a draw method.
+// The flower should look like the picture below:
+// http://i.imgur.com/OC4EA29.png
+
+// The constructor takes the (x, y) coordinate of the upper left-hand
+// corner of the flower. The arrow points to the x,y coordinates of
+// the flower.
+ 
+// There are four pink petals and a yellow center. Each petal and the
+// center is a circle with a diameter of 20. A circle is an ellipse in
+// which the width and height are both equal to the diameter of the
+// circle. The pink circles touch the yellow one.
+
+// The stem is green and is on the center line of the flower. Its
+// length is 3 times the diameter.
