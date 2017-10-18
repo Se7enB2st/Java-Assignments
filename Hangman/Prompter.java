6a9a317dba2169f36711298da104fed5aac9b214
Ethan He <ethanhe1992@gmail.com>
2017-10-18 18:58:33 -0400

    Update Prompter.java

--- Hangman/Prompter.java
+++ Hangman/Prompter.java
@@ -9,13 +9,27 @@ class Prompter {
   
   public boolean promptForGuess() {
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter a letter:  ");
-    String guessInput = scanner.nextLine();
-    char guess = guessInput.charAt(0);
-    return game.applyGuess(guess);    
+    boolean isHit = false; 
+    boolean isAcceptable = false;
+    
+    do{
+      System.out.print("Enter a letter:  ");
+      String guessInput = scanner.nextLine();
+      
+      try{
+        isHit = game.applyGuess(guessInput);
+        isAcceptable = true;
+      } catch(IllegalArgumentException iae){
+        System.out.printf("%s. Please try again. %n",
+                          iae.getMessage());
+      }
+    }while(!isAcceptable);
+    return isHit;
   }
   
   public void displayProgress() {
-    System.out.printf("Displaying current progress: %s\n", game.getCurrentProgress());
-  }
+    System.out.printf("You have %d tries left to solve:  %s%n",
+                      game.getRemainingTries(),
+                      game.getCurrentProgress());  
+  } 
 }
